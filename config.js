3b39eb475c51e29850ad1a06edcebc71b16f7bb5
Lorenzo Jimenez <lorenzojimenez101@yahoo.com>
2018-08-14 06:31:54 -0700

    delete server 2

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 exports.DATABASE_URL =
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    'mongodb://localhost/fortniteDB';
+    'mongodb://localhost/fornitedb';
 exports.PORT = process.env.PORT || 8080;
 
 
